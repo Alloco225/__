7ffed0822d8f37d791e3feebb0b71b6a5c1a4258
Alloco + Ketchup <39595564+Alloco225@users.noreply.github.com>
2021-09-16 16:10:00 +0000

    Update limit_caracters_with_return.dart

--- limit_caracters_with_return.dart
+++ limit_caracters_with_return.dart
@@ -1,15 +1,33 @@
 get observationsFormatted {
-    String r = "";
-    int total = observations.length;
-    int rounds = (total / 50).floor();
-    int remaining = total % 50;
-    int i = 0;
-    while (i < rounds) {
-      r += observations.substring(50 * i, (50 * (i + 1)) - 1);
-      r += "\n";
-      i++;
+    print(">>> obs");
+    print(observations);
+    String r = observations;
+    if (observations != null) {
+      // new version
+      // if (observations.length > 50) {
+      //   return observations.substring(0, 49);
+      // }
+      // return observations;
+
+      int total = observations.length;
+      int rounds = (total / 50).floor();
+      int remaining = total % 50;
+      int i = 0;
+
+      if (rounds == 0) {
+        return r;
+      }
+      r = "";
+      while (i < rounds) {
+        r += observations.substring(50 * i, (50 * (i + 1)) - 1);
+        r += "\n";
+        i++;
+      }
+      // return "$i $remaining ${50 * i},${50 * i + remaining - 1}  ${observations.length}";
+      if(remaining > 0){
+        r += observations.substring(50 * i, 50 * i + remaining - 1);
+      }
     }
-    r += observations.substring(i, remaining);
 
     return r;
   }
