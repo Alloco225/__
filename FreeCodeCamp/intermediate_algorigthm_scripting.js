789d1d9e2ea0f92bbfa800b248ef4f6753dbb42a
Alloco + Ketchup <39595564+Alloco225@users.noreply.github.com>
2022-03-14 19:02:27 +0000

    Update intermediate_algorigthm_scripting.js

--- FreeCodeCamp/intermediate_algorigthm_scripting.js
+++ FreeCodeCamp/intermediate_algorigthm_scripting.js
@@ -12,3 +12,45 @@ function sumAll(arr) {
 
 console.log(sumAll([1, 4]));
 
+
+
+// Diff Two Arrays
+
+function diffArray(arr1, arr2) {
+  const newArr = [];
+  arr2.forEach(i => {
+    if(arr1.every(a => a != i)){
+      newArr.push(i)
+    }
+  })
+  arr1.forEach(i => {
+    if(arr2.every(a => a != i)){
+      newArr.push(i)
+    }
+  })
+  return newArr;
+}
+
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
+
+
+
+// Seek and Destroy
+
+function destroyer(arr, ...args) {
+  [...args].forEach(a => {
+    let elems = arr.filter(v => v == a)
+    elems.forEach(e => arr.splice(arr.indexOf(e), 1))
+  })
+  return arr;
+}
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3));
+console.log(destroyer([3, 5, 1, 2, 2], 2, 3, 5));
+
+
+
+
