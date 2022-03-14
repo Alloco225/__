b77869ebfce9f867c835dabd42bafcbe09e1cc95
Alloco + Ketchup <39595564+Alloco225@users.noreply.github.com>
2022-03-14 19:34:48 +0000

    Update intermediate_algorigthm_scripting.js

--- FreeCodeCamp/intermediate_algorigthm_scripting.js
+++ FreeCodeCamp/intermediate_algorigthm_scripting.js
@@ -53,4 +53,31 @@ console.log(destroyer([3, 5, 1, 2, 2], 2, 3, 5));
 
 
 
+// Wherefore art thou
+
+function whatIsInAName(collection, source) {
+  const arr = [];
+  // Only change code below this line
+  let keys = Object.keys(source);
+
+  collection.forEach(c => {
+  
+    if(keys.every(k =>  Object.keys(c).includes(k) && c[k] == source[k])){
+      arr.push(c)
+    }
+  
+  })
+  console.log(arr)
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 });
+
+
+
+
+
 
