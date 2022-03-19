ca28a699e829f547459b1c678bc21587f024098e
Alloco + Ketchup <39595564+Alloco225@users.noreply.github.com>
2022-03-19 12:39:08 +0000

    Update regular_expressions.js

--- FreeCodeCamp/regular_expressions.js
+++ FreeCodeCamp/regular_expressions.js
@@ -5,3 +5,11 @@ let username = "JackOfAllTrades";
 //let userCheck = /[[a-z]{2}|^[a-z][^0-9]*[0-9]+$]/i; // Change this line
 let userCheck = /^[a-z]{2,}[0-9]*$|^[a-z][0-9]{2,}$/i; // Change this line
 let result = userCheck.test(username);
+
+
+
+// Positive and Negative Lookahead
+
+let sampleWord = "astronaut";
+let pwRegex = /(?=.{6,})(?=.*\d{2}.*)/; // Change this line
+let result = pwRegex.test(sampleWord);
