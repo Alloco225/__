a5055aed1acbce4215d73da1658a1d84e3ba4bfb
Alloco + Ketchup <39595564+Alloco225@users.noreply.github.com>
2022-03-21 22:21:53 +0000

    Update intermediate_algorigthm_scripting.js

--- FreeCodeCamp/intermediate_algorigthm_scripting.js
+++ FreeCodeCamp/intermediate_algorigthm_scripting.js
@@ -117,3 +117,23 @@ console.log(translatePigLatin("consonant"));
 
 
 
+// Search and Replace
+
+function myReplace(str, before, after) {
+  after = before[0] == before[0].toUpperCase() ? after[0].toUpperCase() + after.substr(1) : after.toLowerCase();
+
+  return str.replace(before, after)
+}
+
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
+console.log(myReplace("Let us go to the store", "store", "mall"));
+console.log(myReplace("This has a spellngi error", "spellngi", "spelling"));
+console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"));
+console.log(myReplace("His name is Tom", "Tom", "john"));
+
+
+
+
+
+
+
