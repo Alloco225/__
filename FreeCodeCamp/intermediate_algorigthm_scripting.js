f2925d281dbc3f674577d91b9114ee5f9d2cbbb3
Alloco + Ketchup <39595564+Alloco225@users.noreply.github.com>
2022-03-23 21:21:46 +0000

    Update intermediate_algorigthm_scripting.js

--- FreeCodeCamp/intermediate_algorigthm_scripting.js
+++ FreeCodeCamp/intermediate_algorigthm_scripting.js
@@ -449,5 +449,23 @@ console.log(bob.getFullName());
 
 
 
+// Map the Debris
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  return arr.slice().map(elem => {
+    return {
+      name: elem.name, orbitalPeriod: Math.round(2*Math.PI * Math.sqrt(Math.pow((elem.avgAlt+earthRadius), 3)/GM))
+      }
+  });
+}
+
+console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));
+
+
+
+//
+
 
 
