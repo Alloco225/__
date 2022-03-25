a9cd892da35f898b1bd825cdbbb33019df70af30
Alloco + Ketchup <39595564+Alloco225@users.noreply.github.com>
2022-03-25 16:25:03 +0000

    Update algorith_and_data_structure_projects.js

--- FreeCodeCamp/algorith_and_data_structure_projects.js
+++ FreeCodeCamp/algorith_and_data_structure_projects.js
@@ -158,7 +158,6 @@ convertToRoman(99);
 
 
 
-
 // Cash Register
 
 function checkCashRegister(price, cash, cid) {
@@ -265,7 +264,6 @@ checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], 
 
 
 //checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
-
 
 
 
