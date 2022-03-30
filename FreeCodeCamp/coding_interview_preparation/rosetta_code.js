23204c881293f416eec6132e66f3b25f3b139e7e
Alloco + Ketchup <39595564+Alloco225@users.noreply.github.com>
2022-03-30 14:04:40 +0000

    Update rosetta_code.js

--- FreeCodeCamp/coding_interview_preparation/rosetta_code.js
+++ FreeCodeCamp/coding_interview_preparation/rosetta_code.js
@@ -97,3 +97,94 @@ function ack(m, n) {
 }
 
 
+
+
+
+
+// $$$$$$$$$$$$$$$$$$
+// Align columns
+function formatText(input, justification) {
+  console.log("formatText", justification)
+  let result = input.slice().map(a => a.replace(/\$/g, ' '))
+  let longest_string_length = result.sort((r, b) => { return b.length - r.length })[0].length
+
+  for (let i = 0; i < result.length; i++) {
+    let line = result[i]
+    let number_of_spaces = line.match(/ /g).length
+    let spaces_to_fill = longest_string_length - line.length
+    let number_of_spaces_to_add_per_spot = Math.floor(spaces_to_fill / number_of_spaces);
+
+
+    let extra_space = spaces_to_fill % number_of_spaces;
+
+    //console.log("line", number_of_spaces, "to fill", spaces_to_fill, "spaces/spot", number_of_spaces_to_add_per_spot, "extra:", extra_space)
+
+
+
+
+    let words = line.split(' ')
+    //console.log(">>", words)
+    words = words.map((w, i) => {
+      let spaces = ' '
+      //if (i == 0) {
+        //spaces = ' '.repeat(extra_space)
+      //}
+      if(number_of_spaces_to_add_per_spot > 0){
+        spaces = ' '.repeat(number_of_spaces_to_add_per_spot)
+      }
+      if (i == words.length - 1) {
+        spaces = ''
+      }
+      //console.log("spaces", spaces.length)
+      let format = w;
+      switch (justification) {
+        case "right":
+          format = spaces + w
+          break;
+        case "left":
+          format = w + spaces
+          break;
+        case "center":
+          let c = Math.floor(number_of_spaces_to_add_per_spot/2)
+          if(c > 0){
+            spaces = ' '.repeat()
+          }
+
+          format = spaces + w + spaces
+          break;
+      }
+      return format
+    })
+    //console.log(" wordss", words)
+    // Rejoin words
+    line = words.join('')
+    result[i] = line
+    //console.log(">l", line)
+  }
+  //console.log("\n\nr", result)
+  result.forEach(r => {
+    console.log(r)
+  })
+
+  console.log(" ")
+
+  return result
+}
+
+const testText = [
+  'Given$a$text$file$of$many$lines',
+  'where$fields$within$a$line$',
+  'are$delineated$by$a$single$"dollar"$character',
+  'write$a$program',
+  'that$aligns$each$column$of$fields$',
+  'by$ensuring$that$words$in$each$',
+  'column$are$separated$by$at$least$one$space.',
+  'Further,$allow$for$each$word$in$a$column$to$be$either$left$',
+  'justified,$right$justified',
+  'or$center$justified$within$its$column.'
+];
+
+
+formatText(testText, 'right')
+formatText(testText, 'left')
+formatText(testText, 'center')
