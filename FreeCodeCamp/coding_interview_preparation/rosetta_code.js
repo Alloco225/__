4a05ff4798b02529adc5aab4f81b869cb39daf7f
Alloco + Ketchup <39595564+Alloco225@users.noreply.github.com>
2022-03-30 13:18:32 +0000

    Update rosetta_code.js

--- FreeCodeCamp/coding_interview_preparation/rosetta_code.js
+++ FreeCodeCamp/coding_interview_preparation/rosetta_code.js
@@ -24,3 +24,76 @@ function getFinalOpenedDoors(numDoors) {
 
 console.log(getFinalOpenedDoors(5))
 
+
+
+
+// ***************
+
+// ***************
+
+// ***************
+
+// ***************
+
+
+
+
+
+// ABC Problem
+
+function canMakeWord(word) {
+  const BASE_BLOCKS = [
+    ['B', 'O'],
+    ['X', 'K'],
+    ['D', 'Q'],
+    ['C', 'P'],
+    ['N', 'A'],
+    ['G', 'T'],
+    ['R', 'E'],
+    ['T', 'G'],
+    ['Q', 'D'],
+    ['F', 'S'],
+    ['J', 'W'],
+    ['H', 'U'],
+    ['V', 'I'],
+    ['A', 'N'],
+    ['O', 'B'],
+    ['E', 'R'],
+    ['F', 'S'],
+    ['L', 'Y'],
+    ['P', 'C'],
+    ['Z', 'M'],
+  ]
+
+  let blocks = BASE_BLOCKS.slice();
+
+  let last_letter = 0;
+  for(var l = 0; l < word.length; l++){
+    let letter = word[l]
+    let index = blocks.findIndex(b => b.includes(letter.toUpperCase()))
+    if(~index){
+      //console.log("block", blocks[index], blocks.length)
+      last_letter = l;
+      blocks.splice(index, 1)
+    }else{
+      break
+    }
+  }
+
+  return last_letter == word.length -1
+}
+
+
+console.log(canMakeWord("bark"))
+
+
+
+
+// Ackermann function
+function ack(m, n) {
+  if(m == 0) return n +1
+  if(n == 0) return ack(m-1, 1)
+  return ack(m-1, ack(m, n-1))
+}
+
+
