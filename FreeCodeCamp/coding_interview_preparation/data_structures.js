1df29b7a004a4b70354d8cc407c3fbfeaec3fdbf
Alloco + Ketchup <39595564+Alloco225@users.noreply.github.com>
2022-04-04 15:17:21 +0000

    Update data_structures.js

--- FreeCodeCamp/coding_interview_preparation/data_structures.js
+++ FreeCodeCamp/coding_interview_preparation/data_structures.js
@@ -101,3 +101,57 @@ class Set {
   }
   // Only change code above this line
 }
+
+
+// Perform a Union on Two Sets
+
+class Set {
+  constructor() {
+    // This will hold the set
+    this.dictionary = {};
+    this.length = 0;
+  }
+  // This method will check for the presence of an element and return true or false
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+  // This method will return all the values in the set
+  values() {
+    return Object.values(this.dictionary);
+  }
+  // This method will add an element to the set
+  add(element) {
+    if (!this.has(element)) {
+      this.dictionary[element] = element;
+      this.length++;
+      return true;
+    }
+
+    return false;
+  }
+  // This method will remove an element from a set
+  remove(element) {
+    if (this.has(element)) {
+      delete this.dictionary[element];
+      this.length--;
+      return true;
+    }
+
+    return false;
+  }
+  // This method will return the size of the set
+  size() {
+    return this.length;
+  }
+  // Only change code below this line
+  union(other_set){
+    
+    other_set.values().forEach(v => {
+      if(!this.has(v)){
+        this.add(v)
+      }
+    })
+    return this
+  }
+  // Only change code above this line
+}
