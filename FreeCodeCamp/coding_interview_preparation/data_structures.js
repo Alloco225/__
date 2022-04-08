579fa1fe4f518d6328c60c8a863a6038fc4b6046
Alloco + Ketchup <39595564+Alloco225@users.noreply.github.com>
2022-04-08 15:31:26 +0000

    Update data_structures.js

--- FreeCodeCamp/coding_interview_preparation/data_structures.js
+++ FreeCodeCamp/coding_interview_preparation/data_structures.js
@@ -985,3 +985,74 @@ function LinkedList() {
 }
 
 
+
+// Add Elements at a Specific Index in a Linked List
+
+function LinkedList() {
+  var length = 0;
+  var head = null;
+
+  var Node = function(element) {
+    this.element = element;
+    this.next = null;
+  };
+
+  this.size = function() {
+    return length;
+  };
+
+  this.head = function() {
+    return head;
+  };
+
+  this.add = function(element) {
+    var node = new Node(element);
+    if (head === null) {
+      head = node;
+    } else {
+      var currentNode = head;
+
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+
+      currentNode.next = node;
+    }
+    length++;
+  };
+
+  // Only change code below this line
+  this.addAt = function(index, element){
+    if(index < 0 || index >= length){
+      return false
+    }
+    let current = head
+    let newNode = new Node(element)
+    if(index == 0){
+      head = newNode
+      newNode.next = current
+      length++
+      return true
+    }
+    let i = 0
+    let previous
+    while(current.next){
+      if(i == index){
+        previous.next = newNode
+        newNode.next = current
+      length++
+        break;
+      }
+      previous = current
+      current = current.next
+    }
+    return true
+  }
+  // Only change code above this line
+}
+
+
+
+
+
+
