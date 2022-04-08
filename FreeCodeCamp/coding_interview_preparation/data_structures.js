2085ebb80030abd46d4597d6e5aea64b27f42ff9
Alloco + Ketchup <39595564+Alloco225@users.noreply.github.com>
2022-04-08 15:05:02 +0000

    Update data_structures.js

--- FreeCodeCamp/coding_interview_preparation/data_structures.js
+++ FreeCodeCamp/coding_interview_preparation/data_structures.js
@@ -824,3 +824,99 @@ function LinkedList() {
 
 
 
+// Search within a Linked List
+
+function LinkedList() {
+  var length = 0;
+  var head = null;
+
+  var Node = function(element){
+    this.element = element;
+    this.next = null;
+  };
+
+  this.size = function() {
+    return length;
+  };
+
+  this.head = function(){
+    return head;
+  };
+
+  this.add = function(element){
+    var node = new Node(element);
+    if(head === null){
+        head = node;
+    } else {
+      var currentNode = head;
+
+      while(currentNode.next){
+        currentNode = currentNode.next;
+      }
+
+      currentNode.next = node;
+    }
+
+    length++;
+  };
+
+  this.remove = function(element){
+    var currentNode = head;
+    var previousNode;
+    if(currentNode.element === element){
+      head = currentNode.next;
+    } else {
+      while(currentNode.element !== element) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+
+      previousNode.next = currentNode.next;
+    }
+
+    length --;
+  };
+
+  // Only change code below this line
+  this.isEmpty = function(){
+    return length == 0
+  }
+
+  this.indexOf = function(element){
+    let index = 0
+    let found = false;
+    var current = head
+    while(current.next){
+      if(current.element == element){
+        found = true
+        break;
+      }
+      index++
+    }
+
+    return found ? index : -1
+  }
+
+  this.elementAt = function(index){
+    var i = 0;
+    var element
+    var current = head
+    while(current.next){
+      if(i == index){
+        if(current.element){
+          element = current.element
+        }
+        break;
+      }
+      current = current.next
+      i++
+    }
+  console.log("elementAt", index, element)
+    return element
+  }
+  // Only change code above this line
+}
+
+
+
+
